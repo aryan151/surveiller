d3c237301e0689cffa0ecaa301e5c80a98984cc2
aryan <aryanattula@gmail.com>
2022-01-26 20:11:52 -0500

    theme working

--- react-app/src/components/Navigation/navigation.js
+++ react-app/src/components/Navigation/navigation.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { useSelector } from "react-redux"; 
+import {React, useState} from 'react';
+import { useDispatch, useSelector } from "react-redux";
 import { NavLink, Link } from "react-router-dom"; 
 import LogoutButton from "../auth/LogoutButton";    
 import { MdMenuOpen } from "react-icons/md";
@@ -10,14 +10,25 @@ import { MdOutlineChecklist } from "react-icons/md";
 import { AiOutlineUsergroupAdd } from "react-icons/ai";
 import { MdCases } from "react-icons/md";
 import {toggleTheme} from "../../store/theme"
+import { RiMoonFill } from "react-icons/ri";
+import {FiSun} from "react-icons/fi";
 
 
 import './sidebar.css' 
 
 function Navigation ({show, toggle}) { 
+	const dispatch = useDispatch(); 
     const user = useSelector((state) => state.session.user);   
 	const theme = useSelector(state => state.theme); 
     const navToggle = show ? "NavShow" : "NavClose"; 
+
+	const [currentTheme, setCurrentTheme] = useState(theme);
+
+	function toggleLang() {
+		dispatch(toggleTheme());
+		setCurrentTheme(currentTheme === 'fre' ? 'eng' : 'fre');   
+	}
+
     return (
 		<nav className={navToggle}>  
 			<div className="Navtop"> 
@@ -29,33 +40,34 @@ function Navigation ({show, toggle}) {
 			<div className="sidebar-links-section">
 				<NavLink to="/home" exact={true} activeClassName="sidebar-active">
 					<div id="sidebar-link">
-						<RiHome6Line size="1.5em" /> <span id="sidebar-link-text">Home</span>  
+						<RiHome6Line size="1.5em" /> <span id="sidebar-link-text">{theme === 'eng' ? 'Home' : 'Domicile'}</span>  
 					</div>
 				</NavLink>  
                 <NavLink to="/toDo" exact={true} activeClassName="sidebar-active">
 					<div id="sidebar-link">
-						<MdOutlineChecklist size="1.5em" /> <span id="sidebar-link-text">To Do</span> 
+						<MdOutlineChecklist size="1.5em" /> <span id="sidebar-link-text">{theme === 'eng' ? 'To Do' : 'Liste'}</span> 
 					</div>
 				</NavLink> 
                 <NavLink to="/workers" exact={true} activeClassName="sidebar-active">
 					<div id="sidebar-link">
-						<AiOutlineUsergroupAdd size="1.5em" /> <span id="sidebar-link-text">Employees</span> 
+						<AiOutlineUsergroupAdd size="1.5em" /> <span id="sidebar-link-text">{theme === 'eng' ? 'Employees' : 'Des Employés'}</span> 
 					</div>
 				</NavLink> 
                 <NavLink to="/inventory" exact={true} activeClassName="sidebar-active">
 					<div id="sidebar-link">
-						<MdCases size="1.5em" /> <span id="sidebar-link-text">Inventory</span>
+						<MdCases size="1.5em" /> <span id="sidebar-link-text">{theme === 'eng' ? 'Inventory' : 'Inventaire'}</span>
 					</div>  
 				</NavLink> 
 			</div>
 			<div className="sidebar-projects-section">
 				<div id="sidebar-projects-title">
-					My Projects
+				{theme === 'eng' ? "All Projects" : "Tous Les Projets"} 
 				</div> 
 			</div>
-			<div id="sidebar-link">
-						<AiOutlineUsergroupAdd size="1.5em" /> <span id="sidebar-link-text">Employees</span> 
-					</div>
+			<div className="toggle-theme-button" onClick={toggleLang}> 
+                  {theme === 'fre' ? <RiMoonFill className='dark-toggle'/> : <FiSun className='light-toggle'/>} 
+				  {theme}
+                </div>
 			<div className="sidebar-log-out">  
 				<LogoutButton /> 
 			</div> 
